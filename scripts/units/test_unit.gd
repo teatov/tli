7e6060b046677233760f8d517bd14df6dc380d18
teatov <hlebpoimenigleb@gmail.com>
2024-10-06 00:41:24 +1000

    add test unit

---
+++ scripts/units/test_unit.gd
@@ -0,0 +1,15 @@
+extends Node3D
+class_name TestUnit
+
+var selected: bool = false
+
+@onready var selection_sprite: Sprite3D = $SelectionSprite
+
+
+func _ready() -> void:
+	set_selected(false)
+
+
+func set_selected(on: bool) -> void:
+	selected = on
+	selection_sprite.visible = selected
