a8da79707c6a57e3a32dbe0b96e312b3774eaa38
teatov <hlebpoimenigleb@gmail.com>
2024-10-08 03:06:06 +1000

    create `AudioStreamPlayerPolyphonic`

---
+++ scripts/audio_stream_player_polyphonic.gd
@@ -0,0 +1,23 @@
+extends AudioStreamPlayer3D
+class_name AudioStreamPlayerPolyphonic
+
+var playback: AudioStreamPlaybackPolyphonic
+
+
+func _ready() -> void:
+	stream = AudioStreamPolyphonic.new()
+	play()
+	playback = get_stream_playback()
+
+func play_polyphonic(
+		new_stream: AudioStream,
+		from_offset: float = 0,
+		new_volume_db: float = 0,
+		new_pitch_scale: float = 1.0,
+) -> void:
+	playback.play_stream(
+			new_stream,
+			from_offset,
+			new_volume_db,
+			new_pitch_scale,
+	)
